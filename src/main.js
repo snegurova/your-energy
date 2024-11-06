cb606bc608a12162bbd69571c7df5bb95a9e1200
Igor <aigor9534@gmail.com>
2024-11-06 19:33:15 +0200

    Save local changes before switching branches

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import { route, handleLocation } from './js/router/router';
 import { updateQuote } from './js/quote/quote';
+import { renderCards } from './js/categories/categories-api';
 import './api-example';
 const links = document.querySelectorAll('.router-link');
 links.forEach(link => link.addEventListener('click', route));
 
-handleLocation();
-updateQuote();
+document.addEventListener('DOMContentLoaded', async () => {
+  await handleLocation();
+  updateQuote();
+
+  // Виконуємо renderCards, якщо це головна сторінка
+  if (window.location.pathname === '/') {
+    renderCards();
+  }
+});
