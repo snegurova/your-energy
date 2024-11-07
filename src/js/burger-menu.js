abeb3df6115975a5f522fdfe3f53f3eabecc4ace
Oleh Solomko <solo.oleg1987@gmail.com>
2024-11-07 16:50:27 +0200

    corrections to comments

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -4,19 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const closeMenuButton = document.querySelector('.close-menu');
   const navLinks = document.querySelectorAll('.router-link');
 
-  // За замовчуванням щоб був Home активний
   const homeLink = document.querySelector('a[href="../pages/home.html"]');
   homeLink.classList.add('active');
 
-  // Обробник кліків для навігаційних посилань
-  navLinks.forEach(link => {
-    link.addEventListener('click', e => {
-      navLinks.forEach(l => l.classList.remove('active'));
+  navLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+      navLinks.forEach((l) => l.classList.remove('active'));
       e.target.classList.add('active');
     });
   });
 
-  // Бургер-меню
   burgerMenuButton.addEventListener('click', () => {
     burgerMenuContent.classList.add('open');
     document.body.style.overflow = 'hidden';
