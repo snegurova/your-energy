6c89159c8c898629c39507eedd70d90512a65429
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-11-08 11:12:57 +0200

    update quote function call

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { route, handleLocation } from './js/router/router';
 import { renderExercises } from './js/renderExercises';
-import { updateQuote } from './js/quote/quote';
 import { renderCards } from './js/categories/categories-api';
 import './api-example';
-import './js/burger-menu';import './js/modal/modal';
+import './js/burger-menu';
+import './js/modal/modal';
 
 const links = document.querySelectorAll('.router-link');
 links.forEach((link) => link.addEventListener('click', route));
@@ -20,7 +20,5 @@ document.addEventListener('DOMContentLoaded', async () => {
 });
 
 document.addEventListener('routeUpdated', () => {
-  updateQuote(); // need to call it for all routes, content load in page dynamic
   renderExercises({ page: 1, limit: 10 });
 });
-
