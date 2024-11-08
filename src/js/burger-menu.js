b5a63faf3c6a7308525e002e0fd46116fcd4a77e
Oleh Solomko <solo.oleg1987@gmail.com>
2024-11-08 11:56:40 +0200

    header v0.21 beta

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const navLinks = document.querySelectorAll('.router-link');
   const logoLink = document.querySelector('.logo');
 
-  // Автоматично встановлюємо активний клас для поточного шляху URL
   function setActiveLink() {
     const currentPath = window.location.pathname;
     navLinks.forEach((link) => {
@@ -22,13 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   setActiveLink();
 
-  // Обробка кліків на посиланнях для зміни активного стану
   navLinks.forEach((link) => {
     link.addEventListener('click', (e) => {
       navLinks.forEach((l) => l.classList.remove('active'));
       e.target.classList.add('active');
 
-      // Закриття меню після кліку на посилання в мобільній версії
       if (burgerMenuContent.classList.contains('open')) {
         burgerMenuContent.classList.remove('open');
         document.body.style.overflow = '';
@@ -36,11 +33,10 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
-  // Додавання активного класу при натисканні на лого
   logoLink.addEventListener('click', (e) => {
-    e.preventDefault(); // Запобігаємо звичайному переходу
-    window.history.pushState({}, '', '/'); // Змінюємо шлях без перезавантаження сторінки
-    setActiveLink(); // Оновлюємо активні посилання
+    e.preventDefault();
+    window.history.pushState({}, '', '/');
+    setActiveLink();
   });
 
   burgerMenuButton.addEventListener('click', () => {
