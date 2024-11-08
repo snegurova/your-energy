8d56480900627fdeb7a7bc899b1066739dc8be16
anastasiia <aizatsana15@gmail.com>
2024-11-08 12:37:29 +0200

    FP add favorites

---
+++ src/js/favorites/favorites-api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import {createCard} from './render-functions'
+
+const favoritesData = localStorage.getItem('favorites');
+let favoritesHTML;
+export const getFavoritesHTML = () => { favoritesHTML = document.querySelector('.favorites') };
+
+axios.defaults.baseURL = 'https://your-energy.b.goit.study/api';
+
+export const getFavorites = () => {
+  if (favoritesData) {
+    const objects = JSON.parse(favoritesData);
+    const requests = objects.map(item =>
+      axios.get(`/exercises/${item}`).then(response => response.data)
+    );
+
+    Promise.all(requests)
+      .then(exercisesData => {
+        const cards = exercisesData.map(createCard).join('');
+        favoritesHTML.innerHTML = `<ul class="favorites-section">${cards}</ul>`;
+      })
+  } else {
+    favoritesHTML.innerHTML = `<p>It appears that you haven't added any exercises to your favorites yet.
+    To get started, you can add exercises that you like to your favorites for easier access in the future.
+    </p>`
+  }
+};
