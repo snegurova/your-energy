086d373fcd69b22117af09b2ae5c34baf2d7ec60
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-11-11 13:47:21 +0200

    remove logs

--- src/js/quote/quote.js
+++ src/js/quote/quote.js
@@ -7,21 +7,17 @@ function getQuoteExpireTime() {
 
 function isNeedToUpdate(time) {
   if (!time) {
-    // console.log('expire time not proviede');
     return true;
   }
-  // console.log('exp time', new Date(time), 'current', new Date());
   return time < Date.now();
 }
 
 export async function fetchQuote() {
   // fetch api function
-  // console.log('api call for quote of the days');
   try {
     const apiResult = await api.quotes.getQuote();
     apiResult.expire = getQuoteExpireTime();
     localStorage.setItem('quote', JSON.stringify(apiResult));
-    // console.log('api resut', apiResult);
     return apiResult;
   } catch (e) {
     console.error(e);
@@ -32,13 +28,11 @@ export async function fetchQuote() {
 async function getQuoteInfo() {
   const localQuote = localStorage.getItem('quote');
   if (!localQuote) {
-    // console.log('No quote info..');
     return await fetchQuote();
   }
 
   const quoteInfo = JSON.parse(localQuote);
   if (isNeedToUpdate(quoteInfo.expire)) {
-    // console.log('qu expired');
     return await fetchQuote();
   }
 
@@ -46,11 +40,9 @@ async function getQuoteInfo() {
 }
 
 async function updateQuote() {
-  // console.log('upde q');
   const quoteText = document.querySelector('.js-qofd');
   const quoteAuthor = document.querySelector('.quote-author');
   const quote = await getQuoteInfo();
-  // console.log(quote);
   if (!(quoteText && quoteAuthor)) {
     localStorage.removeItem('quote');
     return;
