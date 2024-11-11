469ae7b1b5e0ccf58cd85a2113230bbc69f7601a
anastasiia <aizatsana15@gmail.com>
2024-11-11 14:52:37 +0200

    FP extended favorites.html

--- src/js/favorites/favorites-api.js
+++ src/js/favorites/favorites-api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { handleRemoveFromFavorites } from '../modal/exerciseHelpers.js';
 import { createExerciseCard } from '../renderExercises.js';
+import { hideLoader, showLoader } from "../loader";
 
 const favoritesHTML = document.querySelector('.favorites');
 
@@ -30,39 +31,47 @@ export const getFavorites = async (params) => {
   const elemPerPage = 6;
   const objects = JSON.parse(favoritesData);
 
-  if (objects && objects.length > 0) {
-    const sorted = objects.toSorted();
+  try {
 
-    if (page !== undefined) {
-      if (page === 1) {
-        const chunk = sorted.slice(0, elemPerPage);
-        displayData(chunk);
+    if (objects && objects.length > 0) {
+      const sorted = objects.toSorted();
+
+      if (page !== undefined) {
+        if (page === 1) {
+          const chunk = sorted.slice(0, elemPerPage);
+          displayData(chunk);
+        }
+
+        if (page !== 1) {
+          const startIndex = page - 1 * elemPerPage;
+          const chunk = sorted.slice(startIndex, startIndex + elemPerPage);
+          displayData(chunk);
+        }
       }
-
-      if (page !== 1) {
-        const startIndex = page - 1 * elemPerPage;
-        const chunk = sorted.slice(startIndex, startIndex + elemPerPage);
-        displayData(chunk);
-      }
-    }
-
-    const requests = sorted.map((item) =>
-      axios.get(`/exercises/${item['_id']}`).then((response) => response.data)
-    );
-    const exercisesData = await Promise.all(requests);
-    const cards = exercisesData
-      .map((card) => createExerciseCard(card, true))
-      .join('');
-    favoritesHTML.innerHTML = `<ul class="favorites-section card-set">${cards}</ul>`;
-  } else {
-    favoritesHTML.innerHTML = `<p class="not-exist">It appears that you haven't added any exercises to your favorites yet.
+      showLoader();
+      const requests = sorted.map((item) =>
+        axios.get(`/exercises/${item['_id']}`).then((response) => response.data)
+      );
+      const exercisesData = await Promise.all(requests);
+      const cards = exercisesData
+        .map((card) => createExerciseCard(card, true))
+        .join('');
+      favoritesHTML.innerHTML = `<ul class="favorites-section card-set">${cards}</ul>`;
+    } else {
+      favoritesHTML.innerHTML = `<p class="not-exist">It appears that you haven't added any exercises to your favorites yet.
     To get started, you can add exercises that you like to your favorites for easier access in the future.
     </p>`;
+    }
+
+
+    const buttons = document.querySelectorAll('.exercises-delete-btn');
+
+    buttons.forEach((button) => {
+      button.addEventListener('click', remove);
+    });
   }
-
-  const buttons = document.querySelectorAll('.exercises-delete-btn');
-
-  buttons.forEach((button) => {
-    button.addEventListener('click', remove);
-  });
+  finally {
+      hideLoader();
+    }
 };
+
