7dc2df65b434b876519633eecf2062fbf350eed3
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-11-11 12:11:22 +0200

    added links limitaion

--- src/js/reloader/reloader.js
+++ src/js/reloader/reloader.js
@@ -8,22 +8,35 @@ import {
 } from '../../main';
 import { getFilters } from '../pages/home';
 
+function updateLinkParams(limit) {
+  const filterLinks = document.querySelectorAll('.filter-link');
+  if (filterLinks) {
+    filterLinks.forEach((link) => {
+      link.dataset.limit = limit;
+    });
+  }
+}
+
 const handleResize = () => {
   const { search } = window.location;
   if (!search) {
     const limit = getCategoriesLimit();
     defaultParams.set(SEARCH_PARAMS.LIMIT, limit);
+    updateLinkParams(limit);
     getFilters(defaultParams, true);
     return;
   }
   if (search.includes(SEARCH_PARAMS.FILTER)) {
     const limit = getCategoriesLimit();
     const url = updateParameter(SEARCH_PARAMS.LIMIT, limit);
+    updateLinkParams(limit);
     pushState(url, {
       isInitPagination: true,
     });
+    return;
   }
   const limit = getExercisesLimit();
+  updateLinkParams(getCategoriesLimit());
   const url = updateParameter(SEARCH_PARAMS.LIMIT, limit);
   pushState(url, {
     isInitPagination: true,
