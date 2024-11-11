f70b8efc83d14910f4836d95a3e69f79f50e0068
Hrebynakha Anatolii <hrebynakha@gmail.com>
2024-11-11 12:53:48 +0200

    chng path

--- src/js/header/burger-menu.js
+++ src/js/header/burger-menu.js
@@ -2,11 +2,10 @@ const navLinks = document.querySelectorAll('.router-link');
 
 export function setActiveLink(base) {
   const currentPath = window.location.pathname;
-  // console.log('Active links call', currentPath);
   navLinks.forEach((link) => {
     const linkTarget = link.getAttribute('href');
     if (linkTarget === './') {
-      currentPath === '/' || currentPath === base
+      currentPath === '/' || currentPath === base + '/'
         ? link.classList.add('active')
         : link.classList.remove('active');
     }
@@ -23,7 +22,6 @@ export function burgerMenuHandler() {
   const burgerMenuButton = document.querySelector('.burger-menu');
   const burgerMenuContent = document.querySelector('.burger-menu-content');
   const closeMenuButton = document.querySelector('.close-menu');
-  // const logoLink = document.querySelector('.icon-logo');
 
   document.addEventListener('click', (e) => {
     if (
@@ -35,32 +33,6 @@ export function burgerMenuHandler() {
       document.body.style.overflow = '';
     }
   });
-
-  /// * What this code do? * ///
-  // navLinks.forEach((link) => {
-  //   link.addEventListener('click', (e) => {
-  //     navLinks.forEach((l) => l.classList.remove('active'));
-  //     e.target.classList.add('active');
-
-  //     if (burgerMenuContent.classList.contains('open')) {
-  //       burgerMenuContent.classList.remove('open');
-  //       document.body.style.overflow = '';
-  //     }
-  //   });
-  // });
-
-  /// ! logo click it html href param <a href="./", you dont need to listen this
-  // if (logoLink) {
-  //   logoLink.addEventListener('click', (e) => {
-  //     // if (burgerMenuContent.classList.contains('open')) {
-  //     //   burgerMenuContent.classList.remove('open');
-  //     //   document.body.style.overflow = '';
-  //     // }
-  //     // ! reloading page remove all clasess
-  //     window.location.href = '/';
-  //   });
-  // }
-
   if (burgerMenuButton) {
     burgerMenuButton.addEventListener('click', () => {
       burgerMenuContent.classList.add('open');
